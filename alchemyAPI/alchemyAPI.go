fd385ddabd92f62b544aa099cf21070fd4659245
Nicholas Lineback <nicholas.lineback@gmail.com>
2014-07-30 22:16:04 -0600

    added reader interface to alchemist constructor

--- alchemyAPI/alchemyAPI.go
+++ alchemyAPI/alchemyAPI.go
@@ -6,19 +6,28 @@ import (
 	"net/http"
 	"os"
 	"io"
+	"io/ioutil"
+	"encoding/json"
+	"errors"
+	"bytes"
 )
 
 type callMap map[string] map[string] string
 
 type apiKey string
 
-var g_apiKey apiKey
-var g_callMap callMap
-var g_baseURL string
+type Alchemist struct {
+	m_key     apiKey
+	m_callMap callMap
+	m_baseURL string
+}
 
-func Init(a_apiKey ...string) {
-	g_apiKey = apiKey(a_apiKey[0])
-	g_callMap = callMap{
+func NewAlchemist(a_apiKey io.Reader) *Alchemist {
+	buff := new(bytes.Buffer)
+	buff.ReadFrom(a_apiKey)
+	alchemist := new(Alchemist)
+	alchemist.m_key = apiKey(buff.String())
+	alchemist.m_callMap = callMap{
 		"sentiment" : map[string]string { 
 			"url"  : "/url/URLGetTextSentiment" ,
 			"text" : "/text/TextGetTextSentiment",
@@ -91,15 +100,164 @@ func Init(a_apiKey ...string) {
 			"text" : "/text/TextGetRankedTaxonomy",
 		},
 	}
-	g_baseURL = "http://access.alchemyapi.com/calls"
+	alchemist.m_baseURL = "http://access.alchemyapi.com/calls"
+	return alchemist
 }
 
-func Print() {
-	fmt.Println(g_apiKey)
-	fmt.Println(g_callMap)
+func (alchemist *Alchemist) Entities(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["entities"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid Entities type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["entities"][flavor], options, nil)
 }
 
-func ImageTagging(flavor string, options url.Values, imageStr string) (*http.Response, error) {
+func (alchemist *Alchemist) Keywords(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["keywords"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid Keywords type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["keywords"][flavor], options, nil)
+}
+
+func (alchemist *Alchemist) Concepts(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["concepts"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid Concepts type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["concepts"][flavor], options, nil)
+}
+
+func (alchemist *Alchemist) Sentiment(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["sentiment"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid Sentiment type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["sentiment"][flavor], options, nil)
+}
+
+func (alchemist *Alchemist) TargetedSentiment(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["sentiment_targeted"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid Targeted Sentiment type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["sentiment_targeted"][flavor], options, nil)
+}
+
+func (alchemist *Alchemist) Text(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["text"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid text type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["text"][flavor], options, nil)
+}
+
+func (alchemist *Alchemist) TextRaw(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["text_raw"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid text_raw type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["text_raw"][flavor], options, nil)
+}
+
+func (alchemist *Alchemist) Author(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["author"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid author type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["author"][flavor], options, nil)
+}
+
+func (alchemist *Alchemist) Language(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["language"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid language type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["language"][flavor], options, nil)
+}
+
+func (alchemist *Alchemist) Title(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["title"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid title type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["title"][flavor], options, nil)
+}
+
+func (alchemist *Alchemist) Relations(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["relations"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid relations type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["relations"][flavor], options, nil)
+}
+
+func (alchemist *Alchemist) Category(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["category"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid category type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["category"][flavor], options, nil)
+}
+
+func (alchemist *Alchemist) Feeds(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["feeds"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid feeds type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["feeds"][flavor], options, nil)
+}
+
+func (alchemist *Alchemist) Microformats(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["microformats"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid microformats type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["microformats"][flavor], options, nil)
+}
+
+func (alchemist *Alchemist) Combined(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["combined"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid combined type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["combined"][flavor], options, nil)
+}
+
+func (alchemist *Alchemist) Taxonomy(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["taxonomy"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid taxonomy type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["taxonomy"][flavor], options, nil)
+}
+
+func (alchemist *Alchemist) ImageExtraction(flavor string, options url.Values, srcStr string) (map[string] interface{}, error) {
+	_, ok := alchemist.m_callMap["image"][flavor]
+	if !ok {
+		return nil, errors.New("Invalid image extraction type!")
+	}
+	options.Add(flavor, srcStr)
+	return alchemist.analyze(alchemist.m_callMap["image"][flavor], options, nil)
+}
+
+func (alchemist *Alchemist) ImageTagging(flavor string, options url.Values, imageStr string) (map[string] interface{}, error) {
 	var imageFile io.Reader = nil
 	var err error
 	if "image" == flavor {
@@ -110,14 +268,14 @@ func ImageTagging(flavor string, options url.Values, imageStr string) (*http.Res
 	} else {
 		options.Add(flavor, imageStr)
 	}
-	return  analyze(g_callMap["imagetagging"][flavor], options, imageFile)
+	return  alchemist.analyze(alchemist.m_callMap["imagetagging"][flavor], options, imageFile)
 }
 
-func analyze(endpoint string, params url.Values, postData io.Reader) (*http.Response, error) {
-	params.Add("apikey", string(g_apiKey))
+func (alchemist *Alchemist) analyze(endpoint string, params url.Values, postData io.Reader) (map[string] interface{}, error) {
+	params.Add("apikey", string(alchemist.m_key))
 	params.Add("outputMode", "json")
 
-	postURL := g_baseURL + endpoint + "?" + params.Encode()
+	postURL := alchemist.m_baseURL + endpoint + "?" + params.Encode()
 	
 	var resp *http.Response
 	var err error
@@ -127,5 +285,11 @@ func analyze(endpoint string, params url.Values, postData io.Reader) (*http.Resp
 	} else {
 		resp, err = http.Get(postURL)
 	}
-	return resp, err
+	contents, _ := ioutil.ReadAll(resp.Body)
+	var jsonMap map[string] interface{}
+	jsonErr := json.Unmarshal(contents, &jsonMap)
+	if jsonErr != nil {
+		return nil, jsonErr
+	}
+	return jsonMap, err
 }
--- alchemyInit/alchemyInit.go
+++ alchemyInit/alchemyInit.go
@@ -4,13 +4,16 @@ import (
 	"github.com/lineback/alchemyapi_go/alchemyAPI"
 	"net/url"
 	"fmt"
-	"io/ioutil"
+	"strings"
 )
 
 func main() {
-	alchemyAPI.Init("d5afecb608d4153b6f86e39d987cb59e0ce99b07")
-	//alchemyAPI.Print()
-	resp , _ := alchemyAPI.ImageTagging("url", url.Values{}, "http://demo1.alchemyapi.com/images/vision/emaxfpo.jpg")
-	contents, _ := ioutil.ReadAll(resp.Body)
-	fmt.Printf("%s\n", string(contents))
+	alchemist := alchemyAPI.NewAlchemist(strings.NewReader("d5afecb608d4153b6f86e39d987cb59e0ce99b07"))
+
+	resp , _ := alchemist.ImageTagging("url", url.Values{}, "http://demo1.alchemyapi.com/images/vision/emaxfpo.jpg")
+	
+	for _, keyword := range resp["imageKeywords"].([]interface{}){
+		fmt.Println("tag:", keyword.(map[string] interface{})["text"])
+		fmt.Println("score:", keyword.(map[string] interface{})["score"])
+	}
 }
